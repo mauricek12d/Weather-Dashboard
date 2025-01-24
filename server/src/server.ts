28f3147e8bdbb74d243a7c1696c67a8451dd6082
Maurice Zuniga <mauricek12d@gmail.com>
2025-01-24 14:35:16 -0500

    changed the route path to the .env folder

--- server/src/server.ts
+++ server/src/server.ts
@@ -2,16 +2,16 @@ import dotenv from 'dotenv';
 import express from 'express';
 import path from 'path';
 import { fileURLToPath } from 'url';
-import weatherRoutes from './routes/api/weatherRoutes.js';
+import Routes from './routes/index.js';
+
+// Equivalent of __dirname for ES modules
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename); 
 
 dotenv.config({ path: path.resolve(__dirname, '../../.env') });
 
 const app = express();
 const PORT = process.env.PORT || 3001;
-
-// Equivalent of __dirname for ES modules
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
 
 // Serve static files of the entire client dist folder
 app.use(express.static(path.join(__dirname, '../client/dist')));
@@ -21,15 +21,8 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 // Middleware to connect the routes
-app.use('/api', weatherRoutes);
+app.use(Routes);
 
-// Middleware to connect the HTML routes
-app.use('*', htmlRoutes);
-
-// Default route to serve the main client application
-app.get('*', (_req, res) => {
-  res.sendFile(path.join(__dirname, '../../client/dist/index.html'));
-});
 
 // Start the server on the port
 app.listen(PORT, () => console.log(`Listening on PORT: ${PORT}`));

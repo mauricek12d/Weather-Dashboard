0b072f651417c2f6bc21f9d2a4ed93d262ab91bd
Maurice Zuniga <mauricek12d@gmail.com>
2025-02-03 11:21:23 -0500

    Changed the script tag in index.html and changed the static path in server.ts to fix the MIME issue

--- server/src/server.ts
+++ server/src/server.ts
@@ -14,7 +14,7 @@ const app = express();
 const PORT = process.env.PORT || 3001;
 
 // Serve static files of the entire client dist folder
-app.use(express.static(path.join(__dirname, '../client/dist')));
+app.use(express.static(path.join(__dirname, '../../client/dist')));
 
 // Middleware for parsing JSON and urlencoded form data
 app.use(express.json());
